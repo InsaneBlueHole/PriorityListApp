c8964a058322abc0fe14098697c2f5ab5a9b0049
Joao Junior <Joaodanieljr@gmail.com>
2020-10-09 09:05:11 -0300

    criacao do addNote

--- app/src/main/java/com/example/listaprioridade/AddNoteActivity.kt
+++ app/src/main/java/com/example/listaprioridade/AddNoteActivity.kt
@@ -1,27 +1,83 @@
 package com.example.listaprioridade
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add_note.*
+import kotlinx.android.synthetic.main.note_item.*
 
 class AddNoteActivity : AppCompatActivity() {
 
-    lateinit var editTextTitle: EditText
-    lateinit var editTextDescription: EditText
-    lateinit var numberPicker:NumberPicker
+    companion object{
+        const val EXTRA_TITLE = "com.example.listaprioridade.EXTRA_TITLE"
+        const val EXTRA_DESCRIPTION = "com.example.listaprioridade.EXTRA_DESCRIPTION"
+        const val EXTRA_PRIORITY = "com.example.listaprioridade.EXTRA_PRIORITY"
+    }
+
+
+    lateinit var editTextTitle: TextView
+    lateinit var editTextDescription: TextView
+    lateinit var numberPicker: NumberPicker
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
-
-        editTextTitle = findViewById(R.id.text_title)
-        editTextDescription = findViewById(R.id.text_description)
-        numberPicker = findViewById(R.id.number_pick_priority)
+        editTextTitle = findViewById(R.id.edit_text_title)
+        editTextDescription = findViewById(R.id.edit_text_description)
+        numberPicker = number_pick_priority
 
         numberPicker.minValue = 1
         numberPicker.maxValue = 10
 
 
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_close_24)
+        setTitle("Add Note")
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater:MenuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.add_note_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       if(item.itemId == R.id.save_note) {
+           saveNote()
+           return true
+       }
+
+       else return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveNote(){
+        val title:String = editTextTitle?.text.toString()
+        val description:String = editTextDescription?.text.toString()
+        val priority:Int = numberPicker.value
+
+        if(title.trim().isEmpty()|| description.trim().isEmpty()){
+            Toast.makeText(this, "Please Insert a title and description",Toast.LENGTH_LONG).show()
+            return
+        }
+
+        var data = Intent()
+        data.putExtra(EXTRA_TITLE, title)
+        data.putExtra(EXTRA_DESCRIPTION, description)
+        data.putExtra(EXTRA_PRIORITY, priority)
+
+        setResult(RESULT_OK, data)
+        finish()
+    }
+
+
+
 }
--- app/src/main/java/com/example/listaprioridade/MainActivity.kt
+++ app/src/main/java/com/example/listaprioridade/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.listaprioridade
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.util.ArrayList
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_add.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddNoteActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_notes)
         val adapter = NoteAdapter(this)
@@ -30,5 +38,24 @@ class MainActivity : AppCompatActivity() {
         noteViewModel.allNotes.observe(this, Observer { notes ->
             notes?.let { adapter.setNotes(it) }
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK){
+
+            val title: String = data?.getStringExtra(AddNoteActivity.EXTRA_TITLE).toString()
+            val description =  data?.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION).toString()
+            val priority = data!!.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1)
+
+            var note = Note(title,description,priority)
+            noteViewModel.insert(note)
+
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_LONG).show()
+        }
+
+        else{
+            Toast.makeText(this, "Note not Saved", Toast.LENGTH_LONG).show()
+        }
     }
 }
